f22a08c7fb8ae06589a1393091557e9396f4d785
Ishanvi M H <145754602+Ishanvimh@users.noreply.github.com>
2025-04-14 13:09:35 +0530

    Updated App.jsx

--- CycleApp/src/App.jsx
+++ CycleApp/src/App.jsx
@@ -1,13 +1,11 @@
+
 import './App.css';
 import React from 'react';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import Onboarding from './pages/Onboarding';
-
-import{auth,googleProvider} from "./firebase";
-import { signInWithPopup } from 'firebase/auth';
 
 function App() {
   return (
@@ -20,42 +18,4 @@ function App() {
     </Router>
   );
 }
-
-const App=()=>{
-  const handleGoogleSignIn=async()=>{
-    try{
-      console.log("Starting Google Sign in....")
-      const result=await signInWithPopup(auth,googleProvider);
-      console.log("Signed in sucessfully...",result);
-      const token=await result.user.getIdToken();
-      const response=await fetch("http://localhost:3000/api/signup",{
-        method: "POST",
-        headers:{
-          "Content-Type":"application/json",
-          Authorization: token,
-        },
-      });
-
-      const userData = await response.json();
-      console.log("User Data:", userData);
-      if(userData.newUser){
-        navigate("/onboarding");
-      }else{
-        navigate("/home");
-      }
-    }catch(error){
-      console.log("Error in Sign-in");
-    }
-
-  }
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <button
-        onClick={handleGoogleSignIn}
-      >
-        Sign In with Google
-      </button>
-    </div>
-  );
-};
 export default App;
