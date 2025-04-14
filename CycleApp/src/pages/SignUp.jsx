4d9bf9fcf589a64d7f4b416eefde6eabf8198b09
Ishanvi M H <145754602+Ishanvimh@users.noreply.github.com>
2025-04-14 12:25:07 +0530

    Create SignUp.jsx

--- CycleApp/src/pages/SignUp.jsx
+++ CycleApp/src/pages/SignUp.jsx
@@ -1,1 +1,50 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth, googleProvider } from "../firebase";
+import { signInWithPopup } from 'firebase/auth';
+import './SignUp.css'; // Optional styling
+
+const SignUp = () => {
+  const navigate = useNavigate();
+
+  const handleGoogleSignIn = async () => {
+    try {
+      console.log("Starting Google Sign in...");
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log("Signed in successfully", result);
+
+      const token = await result.user.getIdToken();
+
+      const response = await fetch("http://localhost:3000/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+
+      const userData = await response.json();
+      console.log("User Data:", userData);
+
+      if (userData.newUser) {
+        navigate("/onboarding");
+      } else {
+        navigate("/home");
+      }
+
+    } catch (error) {
+      console.log("Error in Sign-in:", error);
+    }
+  };
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <button onClick={handleGoogleSignIn}>
+        Sign In with Google
+      </button>
+    </div>
+  );
+};
+
+export default SignUp;
 
